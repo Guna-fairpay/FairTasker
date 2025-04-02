5ef4c86da1bba4fc97ba0dd02d678f4dc3af493e
Guna-fairpay <guna4fairpay@gmail.com>
2025-04-03 02:17:09 +0530

    Refactor Vehicle Notes feature: - Added `vin` parameter to `VehicleNotesAdd`. - Implemented edit, update, delete, save functionalities for vehicle notes. - Added confirmation dialog for deleting notes. - Improved note display and formatting in `VehicleNotesListing`. - Enhanced the VehicleNotesBloc to handle note operations, data fetching, and user interactions. - Updated API endpoints in ApiRepository for vehicle note operations. - Implemented re-fetch functionality after note operations. - Added cancel option for note updates. - Clear form after successful operations. - Update API and UI for the operations.

--- lib/Repository/api_repository.dart
+++ lib/Repository/api_repository.dart
@@ -175,6 +175,10 @@ class APiRepository {
 
   String get _vehicleNotesHistory => "vehicle_notes_history";
 
+  String get _updateNote => "vehicle_status/update_note/";
+
+  String get _deleteNote => "vehicle_status/delete_note/";
+
   int? get _branchId => Session.of.getInt(Str.branchIdPrefText);
 
   String? get _userId => Session.of.getString(Str.userIdPrefText);
@@ -1476,6 +1480,43 @@ Future<Map<String, dynamic>?> getLocations() async {
       String apiUrl = '${Str.LIST_BASE_URL}$_vehicleNotesHistory/$vin';
       Console.of.log(apiUrl);
       final http.Response? response = await _apiClient.callGetMethod(apiUrl);
+      var mapData = await response.mapData;
+      return mapData;
+    } catch (error) {
+      rethrow;
+    }
+  }
+
+  Future<Map<String, dynamic>?> saveVehicleNotes(
+      {required dynamic vin, required Map<String, dynamic>? body}) async {
+    try {
+      String apiUrl = "${Str.LIST_BASE_URL}$_saveNote$vin";
+      final http.Response? response =
+      await _apiClient.callPostMethodWithBody(apiUrl, body: body);
+      var mapData = await response.mapData;
+      return mapData;
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  Future<Map<String, dynamic>?> updateVehicleNotes(
+      {required dynamic id, required Map<String, dynamic>? body}) async {
+    try {
+      String apiUrl = "${Str.LIST_BASE_URL}$_updateNote$id";
+      final http.Response? response =
+      await _apiClient.callPostMethodWithBody(apiUrl, body: body);
+      var mapData = await response.mapData;
+      return mapData;
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  Future<Map<String, dynamic>?> deleteNote(dynamic id) async {
+    try {
+      String apiUrl = "${Str.LIST_BASE_URL}$_deleteNote$id";
+      final http.Response? response = await _apiClient.callDelete(apiUrl);
       var mapData = await response.mapData;
       return mapData;
     } catch (error) {
