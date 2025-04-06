a524c4c76c2d378215caa7376c5ca59816b5f46c
PrabhuC-FP <prabhu4fairpay@outlook.com>
2025-04-06 20:50:19 +0530

    Fix version display and update dependencies: - Updated app version in `pubspec.yaml` to 1.2.5. - Implemented dynamic version retrieval using `package_info_plus` in `drawer_ui.dart`. - Added `package_info_plus` dependency in `pubspec.yaml`. - Changed `todoId` type to String in `edit_todo_rework_ui.dart`. - Added background color for user icon in `custom_vehicle_history_card_view.dart`.

--- lib/UI/Todo/edit_todo/ui/edit_todo_rework_ui.dart
+++ lib/UI/Todo/edit_todo/ui/edit_todo_rework_ui.dart
@@ -23,7 +23,7 @@ class EditTodoReworkUI extends StatelessWidget {
   Widget build(BuildContext context) {
     return BlocProvider(
       create: (context) =>
-          EditToDoBloc()..add(GetEditTodoInitialEvent(todoId: todoId)),
+          EditToDoBloc()..add(GetEditTodoInitialEvent(todoId: "$todoId")),
       child: BlocListener<EditToDoBloc, EditTodoState>(
         listener: (context, state) {
           state.isLoading ? EasyLoading.show() : EasyLoading.dismiss();
