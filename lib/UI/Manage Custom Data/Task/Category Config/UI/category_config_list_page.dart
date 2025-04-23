0bb989b61a005419028b2f95df5adccd5b1843b4
Guna-fairpay <guna4fairpay@gmail.com>
2025-04-23 15:32:23 +0530

    Add handling for empty filtered data in list views: - Added a check for empty filtered responses in `task_listing_page.dart`, `parts_listing_page.dart`, `category_config_list_page.dart`, and `supplies_listing_page.dart`. - Display "No data found" when filtered response is empty, otherwise, display the ListView.separated.

--- lib/UI/Manage Custom Data/Task/Category Config/UI/category_config_list_page.dart
+++ lib/UI/Manage Custom Data/Task/Category Config/UI/category_config_list_page.dart
@@ -17,54 +17,73 @@ class CategoryConfigListPage extends StatelessWidget {
     return BlocBuilder<CategoryConfigBloc, CategoryConfigState>(
       builder: (context, state) => Column(
         children: [
-          ListView.separated(
-              shrinkWrap: true,
-              physics: const NeverScrollableScrollPhysics(),
-              separatorBuilder: (context, index) => const Divider(
-                    height: 0.5,
-                  ),
-              itemCount:
-                  context.watch<CategoryConfigBloc>().filteredResponse.length,
-              itemBuilder: (context, index) {
-                var item =
-                    context.watch<CategoryConfigBloc>().filteredResponse[index];
-                return SafeArea(
-                  minimum: EdgeInsets.symmetric(vertical: 5.h, horizontal: 1.w),
-                  child: Row(
-                      spacing: 5,
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        Expanded(
-                          child: Utils.getText(item['name'] ?? '',
-                              size: 12.sp, overFlow: TextOverflow.visible),
-                        ),
-                        Expanded(child: Utils.getText(item['category_name'] ?? '',)),
-                        Row(
+          context.watch<CategoryConfigBloc>().filteredResponse.isEmpty
+              ? Utils.getText('No data found ', weight: FontWeight.bold)
+              : ListView.separated(
+                  shrinkWrap: true,
+                  physics: const NeverScrollableScrollPhysics(),
+                  separatorBuilder: (context, index) => const Divider(
+                        height: 0.5,
+                      ),
+                  itemCount: context
+                      .watch<CategoryConfigBloc>()
+                      .filteredResponse
+                      .length,
+                  itemBuilder: (context, index) {
+                    var item = context
+                        .watch<CategoryConfigBloc>()
+                        .filteredResponse[index];
+                    return SafeArea(
+                      minimum:
+                          EdgeInsets.symmetric(vertical: 5.h, horizontal: 1.w),
+                      child: Row(
                           spacing: 5,
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          mainAxisSize: MainAxisSize.min,
                           children: [
-                            InkWell(
-                              onTap: () => context.read<CategoryConfigBloc>().add(EditCategoryConfigEvent(data: item)),
-                                child: Icon(Icons.edit_outlined,color: AppC.blue,size: 20.sp,)),
-
-                            InkWell(
-                                onTap: () {
-                                  AskPermissionDialog.show(context,
-                                  title: "Are you sure?",
-                                  description:
-                                  "Do you want to delete this identifier?",
-                                  positiveText: "Yes, delete it!",
-                                  negativeText: "Cancel",
-                                  isReasonRequired: false,
-                                  onPositivePressed: (){}/*context.read<CategoryConfigBloc>().add(DeleteCategoryConfigEvent(data: item)*/);
-                                },
-
-                                child: Icon(Icons.delete_outline,color: AppC.redAccent,size: 20.sp,)),
-                          ],
-                        )
-                      ]),
-                );
-              }),
+                            Expanded(
+                              child: Utils.getText(item['name'] ?? '',
+                                  size: 12.sp, overFlow: TextOverflow.visible),
+                            ),
+                            Expanded(
+                                child: Utils.getText(
+                              item['category_name'] ?? '',
+                            )),
+                            Row(
+                              spacing: 5,
+                              children: [
+                                InkWell(
+                                    onTap: () => context
+                                        .read<CategoryConfigBloc>()
+                                        .add(EditCategoryConfigEvent(
+                                            data: item)),
+                                    child: Icon(
+                                      Icons.edit_outlined,
+                                      color: AppC.blue,
+                                      size: 20.sp,
+                                    )),
+                                InkWell(
+                                    onTap: () {
+                                      AskPermissionDialog.show(context,
+                                          title: "Are you sure?",
+                                          description:
+                                              "Do you want to delete this identifier?",
+                                          positiveText: "Yes, delete it!",
+                                          negativeText: "Cancel",
+                                          isReasonRequired: false,
+                                          onPositivePressed:
+                                              () {} /*context.read<CategoryConfigBloc>().add(DeleteCategoryConfigEvent(data: item)*/);
+                                    },
+                                    child: Icon(
+                                      Icons.delete_outline,
+                                      color: AppC.redAccent,
+                                      size: 20.sp,
+                                    )),
+                              ],
+                            )
+                          ]),
+                    );
+                  }),
           CompactPagination(
             currentPage: context.watch<CategoryConfigBloc>().currentIndex,
             totalPages: (context.watch<CategoryConfigBloc>().totalCount /
