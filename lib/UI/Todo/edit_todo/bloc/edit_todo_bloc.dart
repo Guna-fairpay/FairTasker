1fe0675fc5e80f66047f8df0fed1f14dce822071
Guna-fairpay <guna4fairpay@gmail.com>
2025-04-24 16:05:34 +0530

    Refactor and improve dropdown functionality: - Replaced `dropdownBoxSmallSize` with `dropdownBox` in `task_view_ui.dart` for selecting category and subcategory. - Updated `GetEditTodoInitialEvent` in `edit_todo_bloc.dart` to fetch data before emitting state. - Removed unused import and functions from `utils.dart`, specifically the `dropdownBoxSmallSize` function.

--- lib/UI/Todo/edit_todo/bloc/edit_todo_bloc.dart
+++ lib/UI/Todo/edit_todo/bloc/edit_todo_bloc.dart
@@ -154,6 +154,7 @@ class EditToDoBloc extends Bloc<EditToDoEvent, EditTodoState> {
     on<GetEditTodoInitialEvent>((event, emit) async {
       try {
         emit(state.copyWith(isLoading: true));
+        var todoResponse = await apiRepository.editToDo(event.todoId);
         var partsResponse = await getIt<CommonService>().getPartsList();
         var suppliesResponse = await getIt<CommonService>().getSuppliesList();
         var vehicleResponse = await getIt<CommonService>().getActiveVehicles();
@@ -162,7 +163,6 @@ class EditToDoBloc extends Bloc<EditToDoEvent, EditTodoState> {
         var taskResponse = await getIt<CommonService>().getTaskExpenseData();
         var userGroupResponse = await getIt<CommonService>().getGroupPersons();
         var assignedToResponse = await getIt<CommonService>().getResources();
-        var todoResponse = await apiRepository.editToDo(event.todoId);
         var partsData=List.from(partsResponse);
         var groupVehiclesResponse =
             await getIt<CommonService>().groupVehicles();
