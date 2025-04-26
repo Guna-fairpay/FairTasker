207ee02baf7eb615532dcce7d3355dca2f6bfc7f
PrabhuC-FP <prabhu4fairpay@outlook.com>
2025-04-26 22:23:45 +0530

    Refactor local authentication: - Implemented biometric authentication check and handling within the bloc. - Added success and failure states for authentication. - Enhanced UI to trigger authentication check and display loading states. - Managed error messages from platform exceptions. - Added initial fetch on init - Handled logout code from Ui to separated function

--- lib/Bloc/local_authentication_bloc.dart
+++ lib/Bloc/local_authentication_bloc.dart
@@ -1,11 +1,47 @@
 import 'package:bloc/bloc.dart';
 import 'package:fairpytasker/Event/local_authentication_event.dart';
 import 'package:fairpytasker/State/local_authentication_state.dart';
+import 'package:fairpytasker/core/initializer/common_initializer.dart';
+import 'package:flutter/services.dart';
+import 'package:local_auth/local_auth.dart';
 
 class LocalAuthenticationBloc extends Bloc<LocalAuthenticationEvent, LocalAuthenticationState> {
-  LocalAuthenticationBloc() : super(LocalAuthenticationInitial()) {
-    on<LocalAuthenticationEvent>((event, emit) {
-      // TODO: implement event handler
-    });
+  final LocalAuthentication _auth = LocalAuthentication();
+  List<BiometricType> _availableBiometrics = [];
+  LocalAuthenticationBloc() : super(LocalAuthenticationLoadingState()) {
+    on<LocalAuthenticationInitialEvent>(_onInitialEvent);
+    on<LocalAuthenticationCheckEvent>(_onCheckEvent);
+  }
+
+  void _onInitialEvent(LocalAuthenticationInitialEvent event, Emitter<LocalAuthenticationState> emit) async {
+    try {
+      await getIt<CommonService>().initialFetch();
+      _availableBiometrics = await _auth.getAvailableBiometrics();
+      if (_availableBiometrics.isNotEmpty) {
+        var isAuthenticated = await _auth.authenticate(localizedReason: "Please authenticate to continue");
+        if (isAuthenticated) {
+          emit(LocalAuthenticationSuccessState());
+        } else {
+          emit(LocalAuthenticationFailureState());
+        }
+      }
+    } catch (e) {
+      e is PlatformException ? emit(LocalAuthenticationFailureState(message: e.message)) : emit(LocalAuthenticationFailureState(message: "Authentication failed"));
+    }
+  }
+
+  void _onCheckEvent(LocalAuthenticationCheckEvent event, Emitter<LocalAuthenticationState> emit) async {
+    try {
+      if (_availableBiometrics.isNotEmpty) {
+        var isAuthenticated = await _auth.authenticate(localizedReason: "Please authenticate to continue");
+        if (isAuthenticated) {
+          emit(LocalAuthenticationSuccessState());
+        } else {
+          emit(LocalAuthenticationFailureState());
+        }
+      }
+    } catch (e) {
+      e is PlatformException ? emit(LocalAuthenticationFailureState(message: e.message)) : emit(LocalAuthenticationFailureState(message: "Authentication failed"));
+    }
   }
 }
--- lib/State/local_authentication_state.dart
+++ lib/State/local_authentication_state.dart
@@ -1,10 +1,22 @@
+import 'dart:math';
+
 import 'package:equatable/equatable.dart';
 
 abstract class LocalAuthenticationState extends Equatable {
-  const LocalAuthenticationState();
+  @override
+  List<Object?> get props => [];
 }
 
-class LocalAuthenticationInitial extends LocalAuthenticationState {
+class LocalAuthenticationLoadingState extends LocalAuthenticationState {}
+class LocalAuthenticationSuccessState extends LocalAuthenticationState {
   @override
-  List<Object> get props => [];
+  List<Object?> get props => [Random().nextDouble()];
 }
+class LocalAuthenticationFailureState extends LocalAuthenticationState {
+  final dynamic message;
+  LocalAuthenticationFailureState({this.message});
+  @override
+  List<Object?> get props => [message, Random().nextDouble()];
+}
+
+
