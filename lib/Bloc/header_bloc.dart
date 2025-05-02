8875f2d2ba5727920c5bce602b550778db651815
PrabhuC-FP <prabhu4fairpay@outlook.com>
2025-05-02 21:09:09 +0530

    Implement branch update callback and improve logging in HeaderBloc: - Added a callback to `CommonService().branchUpdate()` to trigger `HeaderInitialEvent` on branch update. - Enhanced logging with `CURRENT_BRANCH_IDS` in addition to the existing `USER_PUNCH_LIST` and `CHECK_IN_OUT` logs. - Added error logging to capture and report errors in `HeaderInitialEvent`.

--- lib/Bloc/header_bloc.dart
+++ lib/Bloc/header_bloc.dart
@@ -17,6 +17,7 @@ class HeaderBloc extends Bloc<HeaderEvent, HeaderState> {
   var checkOutCount = 0;
   final FBroadcast _broadcast = FBroadcast.instance();
   HeaderBloc() : super(HeaderLoadingState()) {
+    getIt<CommonService>().branchUpdate(callback: () => add(HeaderInitialEvent()));
     _broadcast.register(Str.userPunchListRefresh, (value, callback) => add(HeaderInitialEvent()));
    on<HeaderInitialEvent>(_onInitialEvent);
   }
@@ -32,11 +33,13 @@ class HeaderBloc extends Bloc<HeaderEvent, HeaderState> {
       checkInCount = _userPunchList.where((element) => element['end_time'].toString().trim().isNullOrEmpty).length;
       checkOutCount = _userPunchList.where((element) => element['end_time'].toString().trim().isNotNullOrEmpty).length;
       Console.of.log(_userPunchList, name: "USER_PUNCH_LIST");
+      Console.of.log(_currentBranchHrmIds, name: "CURRENT_BRANCH_IDS");
       Console.of.log("$checkInCount/$checkInCount", name: "CHECK_IN_OUT");
       Console.of.log(_userPunchList.map((e) => e['end_time'].toString().trim().isNullOrEmpty).join(", "), name: "USERPUNCH_1");
       Console.of.log(_userPunchList.map((e) => e['end_time'].toString().trim().isNotNullOrEmpty).join(", "), name: "USERPUNCH_2");
       emit(HeaderCommonState());
     } catch (e) {
+      Console.of.error("Error", error: e);
       emit(HeaderErrorState(e));
     }
   }
