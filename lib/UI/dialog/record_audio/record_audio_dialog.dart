ec78eefd4303d2ffb253c27a7d032a49915f0574
PrabhuC-FP <prabhu4fairpay@outlook.com>
2025-05-09 17:11:20 +0530

    feat: Implement audio recording dialog and add timer
    - Added a `toHMS` extension method to `TimeOfDay` for formatting as hours, minutes, and seconds.
    - Implemented a timer in `RecordAudioBloc` to track recording duration, updating a `ValueNotifier`.
    - Updated `AudioPlayerWidget` to include reset and save buttons with corresponding callbacks.
    - Modified `RecordAudioDialog` to display the recording timer value and use `Chip` widgets for record/stop actions.
    - Adjusted the layout of `RecordAudioDialog` to better accommodate the new timer display and buttons.

--- lib/UI/dialog/record_audio/record_audio_dialog.dart
+++ lib/UI/dialog/record_audio/record_audio_dialog.dart
@@ -1,16 +1,22 @@
 import 'dart:io';
 
 import 'package:audioplayers/audioplayers.dart';
+import 'package:date_time/date_time.dart';
 import 'package:fairpytasker/Component/audio_player_widget.dart';
+import 'package:fairpytasker/Component/success_button.dart';
 import 'package:fairpytasker/UI/dialog/record_audio/record_audio_bloc.dart';
 import 'package:fairpytasker/UI/dialog/record_audio/record_audio_events.dart';
 import 'package:fairpytasker/UI/dialog/record_audio/record_audio_states.dart';
 import 'package:fairpytasker/Utilities/num.dart';
 import 'package:fairpytasker/Utilities/utils.dart';
 import 'package:fairpytasker/core/app/extension/context_extension.dart';
+import 'package:fairpytasker/core/app/extension/int_extension.dart';
 import 'package:fairpytasker/core/app/extension/sized_extension.dart';
+import 'package:fairpytasker/core/app/extension/timeday_extension.dart';
+import 'package:fairpytasker/utilities/appC.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class RecordAudioDialog {
   RecordAudioDialog._();
@@ -87,52 +93,98 @@ class _RecordAudioDialogContentView extends StatelessWidget {
                 mainAxisSize: MainAxisSize.min,
                 children: [
                   if ((!context.watch<RecordAudioBloc>().isRecording) &&
-                      (context.watch<RecordAudioBloc>().audio == null))
-                    IconButton(
+                      (context.watch<RecordAudioBloc>().audio == null)) ...[
+                    /*IconButton(
                         onPressed: () => context
                             .read<RecordAudioBloc>()
                             .add(RecordAudioRecordEvent()),
                         icon: const Icon(Icons.keyboard_voice_outlined),
                         color: context.watch<RecordAudioBloc>().isRecording
                             ? Colors.red
-                            : null),
-                  if (context.watch<RecordAudioBloc>().isRecording)
-                    IconButton(
+                            : null),*/
+                    InkWell(
+                      onTap: () => context
+                          .read<RecordAudioBloc>()
+                          .add(RecordAudioRecordEvent()),
+                      child: Chip(
+                          label: const Text('Tap here to record'),
+                          backgroundColor: AppC.lightGrey,
+                          side: BorderSide.none,
+                          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(Num.borderRadiusXLarge)), avatar: Icon(Icons.mic),),
+                    ),
+                  ],
+                  if (context.watch<RecordAudioBloc>().isRecording) ...[
+                    AnimatedContainer(duration: Durations.short4, child: Column(
+                      children: [
+                        ValueListenableBuilder(
+                            valueListenable:
+                            context.read<RecordAudioBloc>().timerValue,
+                            builder: (context, value, child) => Text(
+                                Time.fromSeconds(int.tryParse(value) ?? 0)
+                                    .toHMS())),
+                        InkWell(
+                          onTap: () => context
+                              .read<RecordAudioBloc>()
+                              .add(RecordAudioStopEvent()),
+                          child: Chip(
+                              label: const Text('Tap here to sop'),
+                              backgroundColor: AppC.lightGrey,
+                              side: BorderSide.none,
+                              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(Num.borderRadiusXLarge)), avatar: Icon(Icons.mic_off_rounded, color: AppC.redAccent)),
+                        ),
+                      ],
+                    ),),
+                    /*IconButton(
                         onPressed: () => context
                             .read<RecordAudioBloc>()
                             .add(RecordAudioStopEvent()),
                         icon: const Icon(Icons.mic_off_rounded),
-                        color: Colors.red),
+                        color: Colors.red),*/
+                    /*ValueListenableBuilder(
+                        valueListenable:
+                            context.read<RecordAudioBloc>().timerValue,
+                        builder: (context, value, child) => Text(
+                            Time.fromSeconds(int.tryParse(value) ?? 0)
+                                .toHMS())),*/
+                  ],
                   if (context.watch<RecordAudioBloc>().audio != null)
-                    Transform.scale(
+                    AnimatedContainer(duration: Durations.medium2, child: Transform.scale(
                         scale: 0.8,
                         filterQuality: FilterQuality.low,
                         alignment: Alignment.topCenter,
                         child: AudioPlayerWidget(
+                            onReset: () => context
+                                .read<RecordAudioBloc>()
+                                .add(RecordAudioResetEvent()),
+                            onSave: () => context
+                                .read<RecordAudioBloc>()
+                                .add(RecordAudioSubmitEvent()),
                             source: DeviceFileSource(
                                 context.watch<RecordAudioBloc>().audio?.path ??
-                                    ""))),
-                  if (context.watch<RecordAudioBloc>().audio != null)
+                                    ""))),),
+                  /*if (context.watch<RecordAudioBloc>().audio != null)
                     Row(
                       mainAxisSize: MainAxisSize.max,
                       spacing: 10,
                       children: [
-                        Expanded(
-                            child: Utils.getFilledButton(
-                                "Reset",
-                                () => context
-                                    .read<RecordAudioBloc>()
-                                    .add(RecordAudioResetEvent()),
-                                bgColor: Colors.red,
-                                textColor: Colors.white)),
-                        Expanded(
-                            child: Utils.getFilledButton(
-                                "Submit",
-                                () => context
-                                    .read<RecordAudioBloc>()
-                                    .add(RecordAudioSubmitEvent())))
+                        Spacer(),
+                        Expanded(child: SuccessButton(
+                          icon: Icons.refresh_rounded,
+                          text: "Reset",
+                          backgroundColor: AppC.redAccent,
+                          onPressed: () => context
+                              .read<RecordAudioBloc>()
+                              .add(RecordAudioResetEvent()),
+                        )),
+                        Expanded(child: SuccessButton(
+                          icon: Icons.send_rounded,
+                          text: "Submit",
+                          onPressed: () => context
+                              .read<RecordAudioBloc>()
+                              .add(RecordAudioSubmitEvent()),
+                        )),
                       ],
-                    )
+                    )*/
                 ],
               ),
             ));
