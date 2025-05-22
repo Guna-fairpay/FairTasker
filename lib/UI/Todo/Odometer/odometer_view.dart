4cff9672fad9721e023eca04bf07a39210ec1b3b
Dhineshbabu-fairpay <dhinesh4fairpay@gmail.com>
2025-05-22 11:09:33 +0530

    Refactor: Change autovalidateMode in Odometer view
    The `autovalidateMode` property in the Odometer view's Form widget has been changed from `AutovalidateMode.onUnfocus` to `AutovalidateMode.onUserInteraction`.

--- lib/UI/Todo/Odometer/odometer_view.dart
+++ lib/UI/Todo/Odometer/odometer_view.dart
@@ -46,7 +46,7 @@ class OdometerView extends StatelessWidget {
                 padding: const EdgeInsets.all(10),
                 child: Form(
                   key: formKey,
-                  autovalidateMode: AutovalidateMode.onUnfocus,
+                  autovalidateMode: AutovalidateMode.onUserInteraction,
                   child: Column(
                     spacing: 10,
                     crossAxisAlignment: CrossAxisAlignment.start,
