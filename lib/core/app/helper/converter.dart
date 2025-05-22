45dc744980d2d83ea709ddd62d8267e79ac5067c
PrabhuC-FP <prabhu4fairpay@outlook.com>
2025-05-22 12:59:49 +0530

    Refactor: Update API endpoint and improve null safety in `feedback_repository.dart`
    - Changed the API endpoint for updating feedback from `_addFeedbackComments` to `_updateFeedBackApiUrl` in the `updateFeedback` method.
    - Improved null safety by using the null-aware operator (`?.`) when accessing `response.body` in various methods within `feedback_repository.dart`.
    - Made the `input` parameter in `parseString` and `_ParseArgs` nullable in `converter.dart` to handle potential null inputs gracefully.
    - Updated `_parseHelper` in `converter.dart` to use an empty string as a fallback if `args.input` is null during JSON decoding.

--- lib/core/app/helper/converter.dart
+++ lib/core/app/helper/converter.dart
@@ -9,19 +9,19 @@ import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
 
 /// A generic method to parse a string into a given type T using compute.
-Future<T> parseString<T>(String input, T Function(Map<String, dynamic> json) fromJson) async {
+Future<T> parseString<T>(String? input, T Function(Map<String, dynamic> json) fromJson) async {
   return compute(_parseHelper, _ParseArgs(input, fromJson));
 }
 
 /// Helper function to perform parsing off the main thread.
 T _parseHelper<T>(_ParseArgs<T> args) {
-  final Map<String, dynamic> jsonMap = jsonDecode(args.input);
+  final Map<String, dynamic> jsonMap = jsonDecode(args.input ?? "");
   return args.fromJson(jsonMap);
 }
 
 /// A class to encapsulate arguments for the compute function.
 class _ParseArgs<T> {
-  final String input;
+  final String? input;
   final T Function(Map<String, dynamic>) fromJson;
 
   _ParseArgs(this.input, this.fromJson);
