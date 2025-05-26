742b5eb7599225bfb63f1dfd1915299fbb35e21f
PrabhuC-FP <prabhu4fairpay@outlook.com>
2025-05-26 13:04:55 +0530

    feat: Add validation support to CompactDropDown
    - Added `FormFieldValidator` and `AutovalidateMode` properties to `CompactDropDown` for input validation.
    - Wrapped `DropdownMenu` with `FormField` to enable validation.
    - Display error messages below the dropdown when validation fails.
    - Adjusted border color to indicate error state.

--- lib/Component/compact_drop_down.dart
+++ lib/Component/compact_drop_down.dart
@@ -2,6 +2,7 @@ import 'package:collection/collection.dart';
 import 'package:fairpytasker/Utilities/num.dart';
 import 'package:fairpytasker/core/app/extension/context_extension.dart';
 import 'package:fairpytasker/core/app/extension/sized_extension.dart';
+import 'package:fairpytasker/core/app/helper/helper.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:fairpytasker/Utilities/appC.dart';
 import 'package:flutter/material.dart';
@@ -15,6 +16,8 @@ class CompactDropDown<T extends Object> extends StatelessWidget {
   final ValueChanged<T?>? onChanged;
   final ItemAsString<T>? itemAsString;
   final TextEditingController? controller;
+  final AutovalidateMode? autoValidateMode;
+  final FormFieldValidator<T>? validator;
 
   const CompactDropDown(
       {super.key,
@@ -24,17 +27,82 @@ class CompactDropDown<T extends Object> extends StatelessWidget {
       this.items,
       this.itemAsString,
       this.onChanged,
-      this.helperText});
+      this.helperText,
+        this.autoValidateMode,
+        this.validator
+      });
 
   @override
   Widget build(BuildContext context) {
     var border = OutlineInputBorder(
       borderRadius: BorderRadius.circular(Num.borderRadius),
-      borderSide:
-          const BorderSide(color: AppC.fieldBase, width: Num.borderWidthButton),
+      borderSide: const BorderSide(color:  AppC.fieldBase, width: Num.borderWidthButton),
     );
-    return DropdownMenu<T>(
-      key: key,
+    return (validator != null) ? FormField<T>(
+      initialValue: initialSelection,
+      autovalidateMode: autoValidateMode,
+      validator: validator,
+        builder: (field) {
+          WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
+            if ((initialSelection != null) && (field.hasError)) field.didChange(initialSelection);
+          });
+          var border = OutlineInputBorder(
+            borderRadius: BorderRadius.circular(Num.borderRadius),
+            borderSide: BorderSide(color: (field.hasError) ? AppC.errorTextColor : AppC.fieldBase, width: Num.borderWidthButton),
+          );
+          return Column(
+            spacing: 3,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              DropdownMenu<T>(
+                // key: key,
+                initialSelection: items?.firstWhereOrNull((element) => const DeepCollectionEquality().equals(initialSelection, element)),
+                hintText: hintText,
+                helperText: helperText,
+                enableSearch: (controller != null),
+                textStyle: context.textTheme.labelLarge?.copyWith(overflow: TextOverflow.ellipsis),
+                menuHeight: context.height * 0.3,
+                inputDecorationTheme: InputDecorationTheme(
+                    hintStyle: context.textTheme.labelMedium?.copyWith(color: AppC.grey),
+                    contentPadding: EdgeInsets.symmetric(horizontal: 10.sp),
+                    enabledBorder: border,
+                    isCollapsed: true,
+                    border: border,
+                    isDense: true,
+                    constraints: BoxConstraints(maxHeight: 35.sp)
+                ),
+                menuStyle: MenuStyle(
+                  backgroundColor: WidgetStateProperty.all<Color>(Colors.white),
+                  visualDensity: const VisualDensity(vertical: VisualDensity.minimumDensity),
+                ),
+                keyboardType: TextInputType.text,
+                requestFocusOnTap: (controller != null),
+                controller: controller,
+                searchCallback: (entries, query) => entries.indexWhere((element) => element.value.toString().toLowerCase().contains(query.toLowerCase())),
+                expandedInsets: 0.padding,
+                dropdownMenuEntries: items
+                    ?.map((item) => DropdownMenuEntry<T>(
+                    value: item,
+                    label: itemAsString?.call(item) ?? item.toString()))
+                    .toList() ??
+                    [],
+                onSelected: (value) {
+                  onChanged?.call(value);
+                  field.didChange(value);
+                },
+              ),
+              if (field.hasError)
+                Row(
+                  spacing: 8,
+                  children: [
+                    const SizedBox.shrink(),
+                    Text(field.errorText ?? "", style: CommonHelper.instance.navigatorKey.currentContext?.textTheme.labelMedium?.copyWith(color: AppC.errorTextColor, fontWeight: FontWeight.w100))
+                  ],
+                )
+            ],
+          );
+        }) : DropdownMenu<T>(
+      // key: key,
       initialSelection: items?.firstWhereOrNull((element) => const DeepCollectionEquality().equals(initialSelection, element)),
       hintText: hintText,
       helperText: helperText,
@@ -42,13 +110,13 @@ class CompactDropDown<T extends Object> extends StatelessWidget {
       textStyle: context.textTheme.labelLarge?.copyWith(overflow: TextOverflow.ellipsis),
       menuHeight: context.height * 0.3,
       inputDecorationTheme: InputDecorationTheme(
-        hintStyle: context.textTheme.labelMedium?.copyWith(color: AppC.grey),
-        contentPadding: EdgeInsets.symmetric(horizontal: 10.sp),
-        enabledBorder: border,
-        isCollapsed: true,
-        border: border,
-        isDense: true,
-        constraints: BoxConstraints(maxHeight: 35.sp)
+          hintStyle: context.textTheme.labelMedium?.copyWith(color: AppC.grey),
+          contentPadding: EdgeInsets.symmetric(horizontal: 10.sp),
+          enabledBorder: border,
+          isCollapsed: true,
+          border: border,
+          isDense: true,
+          constraints: BoxConstraints(maxHeight: 35.sp)
       ),
       menuStyle: MenuStyle(
         backgroundColor: WidgetStateProperty.all<Color>(Colors.white),
@@ -60,10 +128,10 @@ class CompactDropDown<T extends Object> extends StatelessWidget {
       searchCallback: (entries, query) => entries.indexWhere((element) => element.value.toString().toLowerCase().contains(query.toLowerCase())),
       expandedInsets: 0.padding,
       dropdownMenuEntries: items
-              ?.map((item) => DropdownMenuEntry<T>(
-                  value: item,
-                  label: itemAsString?.call(item) ?? item.toString()))
-              .toList() ??
+          ?.map((item) => DropdownMenuEntry<T>(
+          value: item,
+          label: itemAsString?.call(item) ?? item.toString()))
+          .toList() ??
           [],
       onSelected: onChanged,
     );
