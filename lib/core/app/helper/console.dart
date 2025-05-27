da1790ec0ce43eeb8d9a983303b8143660e59654
PrabhuC-FP <prabhu4fairpay@outlook.com>
2025-05-27 20:47:03 +0530

    Refactor: Enhance `CompactAlertDialog` and console logging
    - Added `alignment`, `withMaxWidth`, `insetPadding`, `contentPadding`, `titlePadding`, `backgroundColor`, and `shape` properties to `CompactAlertDialog` for greater customization.
    - Made `CompactAlertDialog` content width conditional based on `withMaxWidth`.
    - Modified console logging to use `developer.log` for messages longer than 120 characters and `_logger` for shorter messages.
    - Enabled `ProductionFilter` for console logging in release mode.

--- lib/core/app/helper/console.dart
+++ lib/core/app/helper/console.dart
@@ -9,14 +9,14 @@ class Console {
   bool get _isDebug => kDebugMode;
   final Logger _logger = Logger(
     printer: PrettyPrinter(methodCount: 0, errorMethodCount: 30, colors: true),
-    // filter: (kDebugMode) ? DevelopmentFilter() : ProductionFilter(),
+    filter: (kDebugMode) ? DevelopmentFilter() : ProductionFilter(),
     level: Logger.level,
     // output: MyConsoleOutput()
   );
   void log(dynamic message, {String name = "Console", Object? error, StackTrace? stackTrace}) {
     if (_isDebug) {
-      developer.log("$message", error: error, stackTrace: stackTrace, name: name, time: DateTime.now());
       if ("$message".length > 120) {
+        developer.log("$message", error: error, stackTrace: stackTrace, name: name, time: DateTime.now());
       } else {
         _logger.i("[$name] $message", error: error, stackTrace: stackTrace, time: DateTime.now());
       }
@@ -25,8 +25,8 @@ class Console {
 
   void error(dynamic message, {String name = "Console", Object? error, StackTrace? stackTrace}) {
     if (_isDebug) {
-      developer.log("$message", error: error, stackTrace: stackTrace, name: name, time: DateTime.now());
       if ("$message".length > 120) {
+        developer.log("$message", error: error, stackTrace: stackTrace, name: name, time: DateTime.now());
       } else {
         _logger.e("[$name] $message", error: error, stackTrace: stackTrace, time: DateTime.now());
       }
@@ -35,8 +35,8 @@ class Console {
 
   void debug(dynamic message, {String name = "Console", Object? error, StackTrace? stackTrace}) {
     if (_isDebug) {
-      developer.log("$message", error: error, stackTrace: stackTrace, name: name, time: DateTime.now());
       if ("$message".length > 120) {
+        developer.log("$message", error: error, stackTrace: stackTrace, name: name, time: DateTime.now());
       } else {
         _logger.d("[$name] $message", error: error, stackTrace: stackTrace, time: DateTime.now());
       }
@@ -45,8 +45,8 @@ class Console {
 
   void warning(dynamic message, {String name = "Console", Object? error, StackTrace? stackTrace}) {
     if (_isDebug) {
-      developer.log("$message", error: error, stackTrace: stackTrace, name: name, time: DateTime.now());
       if ("$message".length > 120) {
+        developer.log("$message", error: error, stackTrace: stackTrace, name: name, time: DateTime.now());
       } else {
         _logger.w("[$name] $message", error: error, stackTrace: stackTrace, time: DateTime.now());
       }
