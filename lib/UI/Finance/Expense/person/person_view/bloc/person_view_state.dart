aef99747c10a9ee6c1b4fed08722ff0f3007fe56
Guna-fairpay <guna4fairpay@gmail.com>
2025-06-09 10:35:19 +0530

    feat: Implement person view for expenses
    This commit introduces a new "Person View" feature for managing expenses associated with individuals.
    
    Key changes:
    - Added `PersonViewMainUI`, `PersonViewListingPage`, and `PersonListItemPage` for the UI.
    - Implemented `PersonViewBloc` for state management, including events (`PersonViewEvent`) and states (`PersonViewState`).
    - Added logic for fetching, displaying, adding, editing, deleting, and approving person-specific expenses.
    - Integrated date range filtering for person expenses.
    - Display total approved amount for the selected period.
    - Updated `APiRepository` to include a new method `getPersonExpense` that returns `Map<String, dynamic>?` for better flexibility and error handling.
    - Refactored `PersonExpenseBloc` to utilize the updated `getPersonExpense` method and adapt to `Map<String, dynamic>` response.
    - Added a utility function `getInitials` in `liststring_extension.dart` to extract initials from names.

---
+++ lib/UI/Finance/Expense/person/person_view/bloc/person_view_state.dart
@@ -0,0 +1,35 @@
+part of 'person_view_bloc.dart';
+
+abstract class PersonViewState extends Equatable{
+  @override
+  List<Object?> get props => [];
+}
+
+class LoadingState extends PersonViewState{}
+
+class CommonState extends PersonViewState {
+  @override
+  List<Object?> get props => [Random().nextDouble()];
+}
+
+class ErrorState extends PersonViewState{
+  final dynamic message;
+  ErrorState(this.message);
+  @override
+  List<Object?> get props => [message, Random().nextDouble()];
+}
+
+class SuccessState extends PersonViewState {
+  final dynamic message;
+  SuccessState(this.message);
+  @override
+  List<Object?> get props => [message, Random().nextDouble()];
+}
+
+class AddEditPageState extends PersonViewState {
+  final dynamic model;
+  AddEditPageState({this.model});
+  @override
+  List<Object?> get props => [model, Random().nextDouble()];
+}
+
