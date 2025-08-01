f87694463b6f5fcfadd2dc40854036cd1fa2e29d
Guna-fairpay <guna4fairpay@gmail.com>
2025-08-01 17:49:05 +0530

    Refactor: Adjust UI for vendor notes and details
    - Modified `VendorNotesResourceView` to align items to the start of the cross-axis.
    - Updated `VendorLocationExtras` to a `Column` layout for better arrangement of vendor details and notes.
    - Notes are now displayed below the vendor/lead/channel information when a lead is present, otherwise, they remain inline.

--- lib/Component/tasker_todo_sub_items/vendor_location_extras.dart
+++ lib/Component/tasker_todo_sub_items/vendor_location_extras.dart
@@ -25,16 +25,35 @@ class VendorLocationExtras extends StatelessWidget {
     final leadName = model['lead']?['customer_name'] ?? "";
     final channelName = model['channel']?['channel_name'] ?? (getIt<CommonService>().channels.firstWhereOrNull((element) => element['id'] == model['channel_id'])?['channel_name'] ?? "");
     final String? leadChannelName = hasLead ? leadName : hasChannel ? channelName : null;
-    return Expanded(child: Row(
-      spacing: 10,
+    return Expanded(child: Column(
+      spacing: 5,
+      crossAxisAlignment: CrossAxisAlignment.start,
       mainAxisAlignment: MainAxisAlignment.start,
-      mainAxisSize: MainAxisSize.min,
       children: [
-        if (hasVendorLocation) Flexible(child: GestureDetector(onTapDown: onVendorOrLocation, child: Utils.getText(vendorLocation, size: 12.sp, overFlow: TextOverflow.ellipsis))),
-        if (leadChannelName.isNotNullOrEmpty) Flexible(child: GestureDetector(onTapDown: onLead, child: Utils.getText(leadChannelName ?? "", size: 12.sp, overFlow: TextOverflow.ellipsis))),
-        if (hasVendorInfo) GestureDetector(onTap: onVendorInfo, child: Icon(Icons.info, size: 14.sp, color: Colors.blue)),
-        if (hasNotes) Flexible(
-          child: RichText(
+        Row(
+          spacing: 10,
+          mainAxisAlignment: MainAxisAlignment.start,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            if (hasVendorLocation) Flexible(child: GestureDetector(onTapDown: onVendorOrLocation, child: Utils.getText(vendorLocation, size: 12.sp, overFlow: TextOverflow.ellipsis))),
+            if (leadChannelName.isNotNullOrEmpty) Flexible(child: GestureDetector(onTapDown: onLead, child: Utils.getText(leadChannelName ?? "", size: 12.sp, overFlow: TextOverflow.ellipsis))),
+            if (hasVendorInfo) GestureDetector(onTap: onVendorInfo, child: Icon(Icons.info, size: 14.sp, color: Colors.blue)),
+            if (hasNotes && !hasLead) Flexible(
+              child: RichText(
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
+                  text: TextSpan(
+                      children: [
+                        TextSpan(text: "($parsedNotes)", recognizer: TapGestureRecognizer()..onTapDown = onNotes),
+                        if (hasTimeChangeReason) TextSpan(text: "\t$timeChangeReason", style: context.textTheme.labelMedium?.copyWith(color: null), recognizer: hasEllipsis ? (TapGestureRecognizer()..onTap = ()=> onMore?.call(timeChangeReason)) : null)
+                      ], style: context.textTheme.labelMedium?.copyWith(fontSize: 11.sp, overflow: TextOverflow.ellipsis, color: AppC.appColor))),
+            ),
+            if (hasAddress) Flexible(child: GestureDetector(onTapDown: onAddress, child: Utils.getText("A", weight: FontWeight.bold, size: 13.sp))),
+            const SizedBox.shrink(),
+          ],
+        ),
+        if(hasNotes && hasLead)
+          RichText(
               maxLines: 1,
               overflow: TextOverflow.ellipsis,
               text: TextSpan(
@@ -42,9 +61,6 @@ class VendorLocationExtras extends StatelessWidget {
                     TextSpan(text: "($parsedNotes)", recognizer: TapGestureRecognizer()..onTapDown = onNotes),
                     if (hasTimeChangeReason) TextSpan(text: "\t$timeChangeReason", style: context.textTheme.labelMedium?.copyWith(color: null), recognizer: hasEllipsis ? (TapGestureRecognizer()..onTap = ()=> onMore?.call(timeChangeReason)) : null)
                   ], style: context.textTheme.labelMedium?.copyWith(fontSize: 11.sp, overflow: TextOverflow.ellipsis, color: AppC.appColor))),
-        ),
-        if (hasAddress) Flexible(child: GestureDetector(onTapDown: onAddress, child: Utils.getText("A", weight: FontWeight.bold, size: 13.sp))),
-        const SizedBox.shrink(),
       ],
     ));
   }
